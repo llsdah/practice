3cd75a815fe88c6bd461de75f1a173e00ab900cf
llsdah <77381506+llsdah@users.noreply.github.com>
2022-10-09 21:20:42 +0900

    코딩 앙마 챕터 5 진행중

---
+++ voca/src/Component/Hello.js
@@ -0,0 +1,21 @@
+export default function(){
+    return 
+        <>
+        <div>
+            
+            <h1 style={
+                {
+                    color:"#f00",
+                    borderRight:"12px solid blue",
+                }
+            } > 
+                hello
+            </h1>
+        </div>
+        </>
+}
+
+
+
+
+
